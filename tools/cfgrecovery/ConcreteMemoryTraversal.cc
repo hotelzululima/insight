f99f4d9aee94b93e0171fa99f74e01677fc3d918
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2012-08-08 07:46:30 +0000

    new class for the traversal of concrete memory.
    git-svn-id: https://insight.labri.fr/svn/trunk@128 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ tools/cfgrecovery/ConcreteMemoryTraversal.cc
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2010-2012, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <decoders/binutils/BinutilsDecoder.hh>
+#include "cfgrecovery.hh"
+#include "ConcreteMemoryTraversal.hh"
+
+using namespace std;
+
+struct ArrowCreate : public Microcode::ArrowCreationCallback
+{
+  list<StmtArrow *> arrows;
+
+  void add_node (Microcode *, StmtArrow *a) {
+    arrows.push_back (a);
+  }
+};
+
+ConcreteMemoryTraversal::ConcreteMemoryTraversal (const ConcreteMemory *memory, 
+						  Decoder *decoder)
+  : mem (memory), decoder (decoder), visited (), in_todo (), todo ()
+{
+}
+
+ConcreteMemoryTraversal::~ConcreteMemoryTraversal ()
+{
+}
+
+bool 
+ConcreteMemoryTraversal::can_visit (const ConcreteAddress &loc) const
+{ 
+  address_t addr = loc.get_address ();
+
+  return (visited.find (addr) == visited.end () &&
+	  in_todo.find (addr) == in_todo.end ());
+}
+
+void 
+ConcreteMemoryTraversal::add_to_todo_list (const ConcreteAddress &loc)
+{
+  address_t addr = loc.get_address ();
+  todo.push_back (addr);
+  in_todo.insert (addr);
+}
+
+ConcreteAddress 
+ConcreteMemoryTraversal::take_from_to_todo_list ()
+{
+  ConcreteAddress result = todo.front ();
+  todo.pop_front ();
+  in_todo.erase (result.get_address ());
+
+  return result;
+}
+
+void 
+ConcreteMemoryTraversal::compute (Microcode *mc,
+				  const ConcreteAddress &entrypoint)
+{
+  ArrowCreate cb;
+
+  if (! can_visit (entrypoint))
+    return;
+
+  mc->add_arrow_creation_callback (&cb);
+  add_to_todo_list (entrypoint);
+
+  while (! todo.empty ())
+    {
+      ConcreteAddress addr = take_from_to_todo_list ();
+
+      if (! mem->is_defined (addr) || ! can_visit (addr))
+	continue;
+
+      visited.insert (addr.get_address ());
+      if (verbosity > 1)
+	{
+	  string inst = 
+	    ((binutils::BinutilsDecoder *) decoder)->get_instruction (addr);
+	  cerr << addr << " : " << inst << endl;
+	}
+      ConcreteAddress next = decoder->decode (mc, addr);
+
+      while (! cb.arrows.empty ())
+	{
+	  StmtArrow *a = cb.arrows.front ();
+	  treat_new_arrow (addr, a, next);
+	  cb.arrows.pop_front ();
+        }      
+    }
+
+  mc->remove_arrow_creation_callback (&cb);
+}
---
+++ tools/cfgrecovery/ConcreteMemoryTraversal.hh
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2010-2012, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef CONCRETEMEMORYTRAVERSAL_HH
+# define CONCRETEMEMORYTRAVERSAL_HH
+
+# include <list>
+# include <set>
+
+# include <domains/concrete/ConcreteMemory.hh>
+# include <kernel/Microcode.hh>
+# include <decoders/Decoder.hh>
+
+class ConcreteMemoryTraversal
+{
+public:
+  ConcreteMemoryTraversal (const ConcreteMemory *memory, Decoder *decoder);
+  virtual ~ConcreteMemoryTraversal ();
+
+  virtual void compute (Microcode *mc, const ConcreteAddress &entrypoint);
+
+  virtual bool can_visit (const ConcreteAddress &loc) const;
+
+protected:
+  virtual void treat_new_arrow (const ConcreteAddress &loc,
+				const StmtArrow *a,
+				const ConcreteAddress &next) = 0;
+
+  void add_to_todo_list (const ConcreteAddress &loc);
+  ConcreteAddress take_from_to_todo_list ();
+
+  const ConcreteMemory *mem;
+  Decoder *decoder;
+  std::set<address_t> visited;
+  std::set<address_t> in_todo;
+  std::list<ConcreteAddress> todo;
+};
+
+#endif /* ! CONCRETEMEMORYTRAVERSAL_HH */
