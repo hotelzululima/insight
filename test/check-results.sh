9c47f02b489be23a4a6c5dad4e0aa63eb79a5c91
vincent <vincent@00316523-1ed1-4fdc-9313-76483a3f41dc>
2012-08-17 15:22:06 +0000

    factor out the check-results.sh script
    git-svn-id: https://insight.labri.fr/svn/trunk@154 00316523-1ed1-4fdc-9313-76483a3f41dc

--- test/check-results.sh
+++ test/check-results.sh
@@ -1,12 +1,11 @@
 #!/bin/sh 
 
 if test "x$1" = "x"; then
-    echo "wrong # of arguments" 1>&2
+    echo "Usage: $0 test-name" 1>&2
     exit 1
 fi
 
 TESTNAME="$1"
 REFNAME="${srcdir}/`basename ${TESTNAME}`.result"
 
-exec diff "${TESTNAME}" "${REFNAME}"
-
+exec diff -u "${REFNAME}" "${TESTNAME}"
