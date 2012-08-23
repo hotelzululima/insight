3548e84f6ab3bdbf16bc808f4c30358c25a702aa
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2012-08-23 20:32:13 +0000

    Split kyua tests into their respective directory. This allows to run only parts of tests.
    git-svn-id: https://insight.labri.fr/svn/trunk@187 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ test/domains/symbolic/memory_test.cc
@@ -0,0 +1,151 @@
+/*-
+ * Copyright (C) 2010-2012, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <atf-c++.hpp>
+
+#include <kernel/expressions/exprutils.hh>
+#include <domains/symbolic/SymbolicMemory.hh>
+#include <kernel/Architecture.hh>
+#include <kernel/insight.hh>
+
+ATF_TEST_CASE(registers);
+ATF_TEST_CASE_HEAD(registers)
+{
+  set_md_var("descr",
+	     "Check the registers behavior within a SymbolicMemory object");
+}
+ATF_TEST_CASE_BODY(registers)
+{
+  insight::init ();
+
+  const Architecture * arch_x86 =
+    Architecture::getArchitecture(Architecture::X86_32);
+
+  ConcreteMemory *cm = new ConcreteMemory;
+  SymbolicMemory * memory = new SymbolicMemory (cm);
+
+  const RegisterDesc * eax = arch_x86->get_register("eax");
+
+  /* Check if an exception is thrown on inexistant register name */
+  ATF_REQUIRE_THROW(Architecture::RegisterDescNotFound,
+		    arch_x86->get_register("xxx"));
+
+  /* Check if register are undefined */
+  ATF_REQUIRE_EQ(memory->is_defined(eax), false);
+
+  /* Check if an exception is thrown on accessing an undefined register */
+  ATF_REQUIRE_THROW(UndefinedValueException, memory->get(eax));
+
+  /* Assigning '00000000000000001000000000000000' to eax */
+  memory->put(eax, SymbolicValue(32, 32768));
+
+  /* Check if register are undefined */
+  ATF_REQUIRE_EQ(memory->is_defined(eax), true);
+
+  /* Check if the put() did work well */
+  ATF_REQUIRE(memory->get(eax) == SymbolicValue(32, 32768));
+
+  delete memory;
+  delete cm;
+
+  insight::terminate ();
+}
+
+ATF_TEST_CASE(memcells);
+ATF_TEST_CASE_HEAD(memcells)
+{
+  set_md_var("descr",
+	     "Check the memory cells behavior within a SymbolicMemory object");
+}
+
+static word_t 
+s_get_simplified (const SymbolicMemory *mem, const ConcreteAddress &a, 
+		  int size_in_bytes, Architecture::endianness_t e)
+{
+  Expr *val = mem->get(a, size_in_bytes, e).get_Expr ()->ref ();
+  exprutils::simplify (&val);
+  ATF_REQUIRE (val->is_Constant ());
+  word_t result = dynamic_cast<const Constant *>(val)->get_val ();
+  val->deref ();
+
+  return result;
+}
+
+ATF_TEST_CASE_BODY(memcells)
+{
+  insight::init ();
+
+  ConcreteMemory *cm = new ConcreteMemory;
+  SymbolicMemory * memory = new SymbolicMemory (cm);
+
+  /* Check if memcells are undefined */
+  ATF_REQUIRE_EQ(memory->is_defined(ConcreteAddress::null_addr()), false);
+  ATF_REQUIRE_EQ(memory->is_defined(ConcreteAddress(6234)), false);
+
+  ConcreteAddress addr = ConcreteAddress(1024);
+  SymbolicValue value = SymbolicValue(32, 6235);
+
+  memory->put(addr, value, Architecture::LittleEndian);
+
+  /* Check if the put() did work well */
+  ATF_REQUIRE_EQ(s_get_simplified (memory, 
+				   addr, 4, Architecture::LittleEndian), 6235);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    addr, 2, Architecture::LittleEndian), 6235);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    addr, 1, Architecture::LittleEndian), 91);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    ++(++addr), 2, Architecture::LittleEndian),
+		  0);
+
+  memory->put(addr, value, Architecture::BigEndian);
+
+  /* Check if the put() did work well */
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    addr, 4, Architecture::BigEndian), 6235);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    addr, 2, Architecture::BigEndian), 0);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    ++(++addr), 2, Architecture::BigEndian), 
+		  6235);
+  ATF_REQUIRE_EQ (s_get_simplified (memory, 
+				    ++addr, 1, Architecture::BigEndian), 91);
+
+  delete memory;
+  delete cm;
+
+  insight::terminate ();
+}
+
+ATF_INIT_TEST_CASES(tcs)
+{
+  ATF_ADD_TEST_CASE(tcs, registers);
+  ATF_ADD_TEST_CASE(tcs, memcells);
+}
---
+++ test/domains/symbolic/simulator_test.cc
@@ -0,0 +1,186 @@
+/*-
+ * Copyright (C) 2010-2012, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <atf-c++.hpp>
+#include <string>
+#include <sstream>
+
+#include <analyses/microcode_exec.hh>
+#include <decoders/DecoderFactory.hh>
+#include <domains/symbolic/SymbolicExecContext.hh>
+#include <kernel/insight.hh>
+#include <kernel/Microcode.hh>
+#include <io/binary/BinutilsBinaryLoader.hh>
+#include <utils/Log.hh>
+
+
+#ifndef TEST_SAMPLES_DIR
+# error TEST_SAMPLES_DIR is not defined
+#endif
+
+using namespace std;
+
+#define EXCEPTION_HANDLING_ADDR 0x12FA792
+#define FAILURE_ADDR 0x6666
+#define SUCCESS_ADDR 0x1111
+
+static void 
+s_simulate (const char *filename)
+{
+  ConfigTable ct;
+  ct.set ("log.debug.enabled", false);
+  ct.set ("log.stdio.enabled", true);
+
+  insight::init (ct);
+
+  BinaryLoader *loader = new BinutilsBinaryLoader (filename);
+  ConcreteMemory *memory = loader->get_memory();
+  const Architecture *A = loader->get_architecture ();
+  // Must initialize registers
+  for (RegisterSpecs::const_iterator i = A->get_registers ()->begin ();
+       i != A->get_registers ()->end (); i++)
+    {
+      if (! i->second->is_alias ())
+	memory->put (i->second, 		     
+		     ConcreteValue (i->second->get_register_size (), 0) );
+    }
+  MicrocodeArchitecture arch (A);
+
+  Decoder *decoder = DecoderFactory::get_Decoder (&arch, memory);
+  ConcreteAddress start = loader->get_entrypoint ();
+
+  log::display << "Entry-point := " << start << endl;
+  SymbolicExecContext *ctxt = new SymbolicExecContext (memory, decoder);
+
+  ctxt->init (SymbolicContext::empty_context (memory));
+
+  MicrocodeAddress lastaddr;
+  MicrocodeAddress newaddr = ctxt->get_current_program_point ().to_address ();
+  SymbolicExecContext::Context *last_context = NULL;
+
+  for (;;)
+    {      
+      lastaddr = newaddr;
+
+      bool simulation_can_continue = ctxt->step();
+
+      if (!(simulation_can_continue && 
+	    ctxt->get_current_context ().hasValue ()))
+	break;
+
+      last_context =  ctxt->get_current_context ().getValue ();
+      last_context->memory->output_text (log::display);
+      log::display << endl;
+
+      ATF_REQUIRE (simulation_can_continue);
+      ATF_REQUIRE (ctxt->pending_arrows.size () > 0);
+
+      newaddr = ctxt->get_current_program_point ().to_address ();
+      
+      if (lastaddr.equals (newaddr))
+	{
+	  SymbolicExecContext::Arrow pa = ctxt->pending_arrows.front ();
+
+	  if (! pa.arr->is_static ())
+	    continue;
+	  
+	  StaticArrow *a = dynamic_cast<StaticArrow *> (pa.arr);
+	  if (a->get_condition () != NULL)
+	    {
+	      SymbolicExecContext::Context *c = 
+		ctxt->get_current_context ().getValue ();
+	      SymbolicValue v = c->eval (a->get_condition ());
+	      if (! v.to_bool().getValue ())
+		continue;
+	    }
+	  if (a->get_target().equals (lastaddr))
+	    break;
+	}
+    }
+
+  ATF_REQUIRE (last_context != NULL);
+  
+  ConcreteAddress exception_handling_addr (EXCEPTION_HANDLING_ADDR);
+  ATF_REQUIRE (last_context->memory->is_defined (exception_handling_addr));
+  
+  SymbolicValue check_exception = 
+    last_context->memory->get (exception_handling_addr, 1, 
+			       arch.get_reference_arch ()->endianness);
+
+  if (! check_exception.to_bool().getValue ())
+    {
+      ATF_REQUIRE (lastaddr.getLocal () == 0);
+      ATF_REQUIRE (lastaddr.getGlobal () == FAILURE_ADDR ||
+		   lastaddr.getGlobal () == SUCCESS_ADDR);
+      ATF_REQUIRE (lastaddr.getGlobal () == SUCCESS_ADDR);
+    }
+  else
+    {
+      ATF_REQUIRE (lastaddr.getGlobal () != FAILURE_ADDR);
+      ATF_REQUIRE (lastaddr.getGlobal () != SUCCESS_ADDR);
+      ATF_REQUIRE (ctxt->pending_arrows.size () == 0);
+    }
+  
+  delete ctxt; 
+  delete loader;
+  delete decoder;
+  delete memory;
+
+  insight::terminate ();
+
+}
+
+#include "simulator_test_cases.hh"
+
+#define BINARY_FILE(id, file) \
+ATF_TEST_CASE(id); \
+\
+ATF_TEST_CASE_HEAD(id)	\
+{ \
+  set_md_var ("descr", \
+	      "Simulate Microcode on binary file '" \
+	      TEST_SAMPLES_DIR file "'"); \
+} \
+\
+ATF_TEST_CASE_BODY(id) \
+{ \
+  s_simulate (TEST_SAMPLES_DIR file); \
+}
+
+SIMULATED_BINARIES
+#undef BINARY_FILE
+
+#define BINARY_FILE(id, file) \
+  ATF_ADD_TEST_CASE(tcs, id);
+
+ATF_INIT_TEST_CASES(tcs)
+{
+  SIMULATED_BINARIES
+}
