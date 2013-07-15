1e714719d6629b3b5e66cd92c3682c3511f4fd92
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-07-15 12:48:47 +0000

    AbstractState: encodes a state of the microcode i.e. a ProgramPoint and a Context.
    git-svn-id: https://insight.labri.fr/svn/trunk@356 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ src/analyses/cfgrecovery/AbstractState.hh
@@ -0,0 +1,32 @@
+#ifndef ABSTRACTSTATE_HH
+# define ABSTRACTSTATE_HH
+
+# include <analyses/cfgrecovery/AbstractProgramPoint.hh>
+# include <analyses/cfgrecovery/AbstractContext.hh>
+
+template<typename PP, typename CTX>
+class AbstractState : public Object 
+{
+public:
+  typedef PP ProgramPoint;
+  typedef CTX Context;
+
+  AbstractState (ProgramPoint *pp, Context *ctx);
+
+  virtual ~AbstractState ();
+
+  virtual ProgramPoint *get_ProgramPoint () const;
+  virtual Context *get_Context () const;
+  virtual bool equals (const AbstractState<ProgramPoint,Context> *s) const;
+  virtual std::size_t hashcode () const;
+  virtual void output_text (std::ostream &out) const;
+  virtual AbstractState<PP,CTX> *clone () const;
+
+private:
+  ProgramPoint *program_point;
+  Context *context;
+};
+
+#include <analyses/cfgrecovery/AbstractState.ii>
+
+#endif /* ! ABSTRACTSTATE_HH */
