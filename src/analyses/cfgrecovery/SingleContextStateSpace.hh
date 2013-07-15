af8ebf36cae275ba51dfeb42db86d84c0c2f78a7
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-07-15 12:48:57 +0000

    SingleContextStateSpace: Simple hashtable used to store states. No merge is done when a program point is encountered several times.
    git-svn-id: https://insight.labri.fr/svn/trunk@365 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ src/analyses/cfgrecovery/SingleContextStateSpace.hh
@@ -0,0 +1,30 @@
+#ifndef SINGLECONTEXTSTATESPACE_HH
+# define SINGLECONTEXTSTATESPACE_HH
+
+# include <tr1/unordered_set>
+# include <set>
+# include <utils/map-helpers.hh>
+# include <analyses/cfgrecovery/AbstractStateSpace.hh>
+
+template <typename State>
+class SingleContextStateSpace : public AbstractStateSpace<State>
+{
+
+public:
+  SingleContextStateSpace ();
+
+  virtual ~SingleContextStateSpace ();
+
+  virtual const State *find_or_add_state (State *s);
+  virtual std::size_t size () const;
+
+private:
+  typedef std::tr1::unordered_set<State *, HashPtrFunctor<State>, 
+  				  EqualsPtrFunctor<State> > StateTable;
+
+  StateTable states;
+};
+
+# include <analyses/cfgrecovery/SingleContextStateSpace.ii>
+
+#endif /* ! SINGLECONTEXTSTATESPACE_HH */
