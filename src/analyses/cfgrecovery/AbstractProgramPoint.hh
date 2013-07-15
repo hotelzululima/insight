9e150565871b8e58d23e432aa6b211f1f02db139
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-07-15 12:48:44 +0000

    AbstractProgramPoint: represents a way to point to a microcode address.
    git-svn-id: https://insight.labri.fr/svn/trunk@354 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ src/analyses/cfgrecovery/AbstractProgramPoint.hh
@@ -0,0 +1,35 @@
+#ifndef ABSTRACTPROGRAMPOINT_HH
+# define ABSTRACTPROGRAMPOINT_HH
+
+# include <utils/Object.hh>
+# include <kernel/Microcode.hh>
+
+template<typename PP>
+class AbstractProgramPoint : public Object
+{
+protected:
+  AbstractProgramPoint ();
+  virtual ~AbstractProgramPoint ();
+
+public:
+  void ref () const;
+
+  void deref ();
+
+  virtual MicrocodeAddress to_MicrocodeAddress () const = 0;
+
+  virtual PP *next (const MicrocodeAddress &addr) const = 0;
+
+  virtual bool equals (const PP *) const = 0;
+
+  virtual std::size_t hashcode () const = 0;
+
+  virtual void output_text (std::ostream &) const = 0;
+
+private:
+  mutable int refcount;
+};
+
+# include <analyses/cfgrecovery/AbstractProgramPoint.ii>
+
+#endif /* ! ABSTRACTPROGRAMPOINT_HH */
