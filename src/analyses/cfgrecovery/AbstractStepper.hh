c61e4a4c3da6e4261dde78dd4e14312879e33eca
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-07-15 12:48:49 +0000

    AbstractStepper: abstract representation of basic methods used to simulate microde: getting the initial state and making one step beyond.
    git-svn-id: https://insight.labri.fr/svn/trunk@358 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ src/analyses/cfgrecovery/AbstractStepper.hh
@@ -0,0 +1,24 @@
+#ifndef ABSTRACTSTEPPER_HH
+# define ABSTRACTSTEPPER_HH
+
+# include <set>
+# include <kernel/Microcode.hh>
+# include <analyses/cfgrecovery/AbstractState.hh>
+
+template<typename S>
+class AbstractStepper 
+{
+public:
+  typedef S State;
+  typedef typename S::ProgramPoint ProgramPoint;
+  typedef typename S::Context Context;
+  typedef std::set<State *> StateSet;
+
+  virtual ~AbstractStepper () { }
+
+  virtual State *get_initial_state (const ConcreteAddress &entrypoint) = 0;
+
+  virtual StateSet *get_successors (const State *s, const StmtArrow *arrow) = 0;
+};
+
+#endif /* ! ABSTRACTSTEPPER_HH */
