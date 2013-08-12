0657d08934cabeacdf93fcc86a7f0d8ae5f0b70e
fleury <fleury@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-08-12 17:51:21 +0000

    Adding a test file for push16
    git-svn-id: https://insight.labri.fr/svn/trunk@490 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ test/test-samples/x86_64-push16.s
@@ -0,0 +1,41 @@
+	## PUSH r/m16
+	push %ax
+	data16 push 0x31415926
+	
+	## PUSH r/m32
+	# push %eax # Not valid in 64-bits mode
+	
+	## PUSH r/m64
+	push  %rax
+	pushq %rax
+	pushq 0x31415926
+	
+	## PUSH r16
+	push %bp
+
+	## PUSH r32
+	# push %esp # Not valid in 64-bits mode
+	# data16 push %esp # Not valid in 64-bits mode
+
+	## PUSH r64
+	push %rsp
+	pushq %rsp
+	
+	## PUSH imm8
+	data16 push $0x31
+	## PUSH imm16
+	data16 push $0x3141
+	## PUSH imm32
+	# data16 push $0x31415926 # Not valid in 64-bits mode
+	## PUSH imm64
+	data16 pushq $0x31415926
+	
+	## PUSH {CS,SS,DS,ES,FS,GS}
+	# data16 push %cs # Not valid in 64-bits mode
+	# data16 push %ss # Not valid in 64-bits mode
+	# data16 push %ds # Not valid in 64-bits mode
+	# data16 push %ds # Not valid in 64-bits mode
+	data16 push %fs
+	data16 push %gs
+
+	
