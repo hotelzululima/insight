39438f65411fa8656d01e0737f1decb3d86b97c1
fleury <fleury@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-09-10 07:41:56 +0000

    Adding tests for sets domain
    git-svn-id: https://insight.labri.fr/svn/trunk@515 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ test/domains/sets/sets_test.cc
@@ -0,0 +1,67 @@
+/*-
+ * Copyright (C) 2010-2013, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <atf-c++.hpp>
+
+#include <domains/sets/SetsValue.hh>
+
+ATF_TEST_CASE(set_test)
+ATF_TEST_CASE_HEAD(set_test)
+{
+  set_md_var("descr",
+	     "Check the Set operations");
+}
+ATF_TEST_CASE_BODY(set_test)
+{
+  /* Checking addition operation */
+  SetsValue my_set();
+  ATF_REQUIRE_EQ(my_set.add_value(ConcreteValue(32, 2)), true);
+  ATF_REQUIRE_EQ(my_set.add_value(ConcreteValue(32, 2)), true);
+  ATF_REQUIRE_EQ(my_set.add_value(ConcreteValue(32, 2)), true);
+
+  delete my_set;
+}
+
+ATF_TEST_CASE(kset_test)
+ATF_TEST_CASE_HEAD(kset_test)
+{
+  set_md_var("descr",
+	     "Check the KSet operations");
+}
+ATF_TEST_CASE_BODY(kset_test)
+{
+
+}
+
+ATF_INIT_TEST_CASES(tcs)
+{
+  ATF_ADD_TEST_CASE(tcs, set_test);
+  ATF_ADD_TEST_CASE(tcs, kset_test);
+}
