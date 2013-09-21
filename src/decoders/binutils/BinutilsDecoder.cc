ddc0d967c1b5cc0d6bc399837bd290e2ed1f65be
fleury <fleury@00316523-1ed1-4fdc-9313-76483a3f41dc>
2013-09-21 09:24:14 +0000

    Freeing memory more appropriately
    git-svn-id: https://insight.labri.fr/svn/trunk@521 00316523-1ed1-4fdc-9313-76483a3f41dc

--- src/decoders/binutils/BinutilsDecoder.cc
+++ src/decoders/binutils/BinutilsDecoder.cc
@@ -322,7 +322,7 @@ s_binutils_sprintf(stringstream *sstream, const char *format, ...)
       if (tmp == NULL)
         {
           free(xbuffer);
-	  free (buffer);
+	  delete (buffer);
           throw Decoder::DecoderUnexpectedError("out of memory");
         }
       else
