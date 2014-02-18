4f6f31eb7f2094f3f9875874310e58b005f85fa7
vincent <vincent@00316523-1ed1-4fdc-9313-76483a3f41dc>
2014-02-18 10:54:17 +0000

    introduce a file which makes std::unordered_map and std::unordered_set available in the std:: namespace, independent of the compiler version we are using
    git-svn-id: https://insight.labri.fr/svn/trunk@617 00316523-1ed1-4fdc-9313-76483a3f41dc

---
+++ src/utils/unordered11.hh
@@ -0,0 +1,53 @@
+/*-
+ * Copyright (C) 2010-2014, Centre National de la Recherche Scientifique,
+ *                          Institut Polytechnique de Bordeaux,
+ *                          Universite Bordeaux 1.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#ifndef UTILS_UNORDERED11_HH_
+#define UTILS_UNORDERED11_HH_
+
+#ifdef HAVE_TR1_UNORDERED_MAP
+
+#define USE_TR1_NAMESPACE
+
+#include <tr1/unordered_map>
+#include <tr1/unordered_set>
+
+namespace std {
+  using namespace tr1;
+}
+
+#else // !HAVE_TR1_UNORDERED_MAP
+
+#undef USE_TR1_NAMESPACE
+
+#include <unordered_map>
+#include <unordered_set>
+
+#endif // !HAVE_TR1_UNORDERED_MAP
+
+#endif /* UTILS_UNORDERED11_HH_ */
