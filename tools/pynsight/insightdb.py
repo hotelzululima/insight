456e7dedb40f3417d596916e4afd32fb031ee438
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2014-03-18 15:59:07 +0000

    Fix doc and typos.
    git-svn-id: https://insight.labri.fr/svn/trunk@703 00316523-1ed1-4fdc-9313-76483a3f41dc

--- tools/pynsight/insightdb.py
+++ tools/pynsight/insightdb.py
@@ -1,65 +1,74 @@
-import insight.debugger 
+import insight.debugger
 from insight.debugger import *
 import argparse
 import code
 
-# change Python path to load modules from working directory 
-sys.path += [ '.' ]
+# change Python path to load modules from working directory
+sys.path += ['.']
 # change the prompt
 sys.ps1 = "insightdb> "
 
-banner = """Insight-based Debugger
-Try help (insight.debugger) to get information on debugger commands.
+banner = """Insight Debugger
+Try 'help(insight.debugger)' to get information on debugger commands.
+Type 'aliases()' to display list of defined aliases.
 """
+
 
 #
 # command-line arguments
 #
-parser = argparse.ArgumentParser (prog="insighdb")
-parser.add_argument ('-b', '--target', help='enforce BFD target', 
-                     dest = "target", default = "", 
-                     required = False, 
-                     metavar ="bfd-target-name")
+parser = argparse.ArgumentParser(prog="insighdb")
+parser.add_argument('-b', '--target', help='enforce BFD target',
+                     dest="target", default="",
+                     required=False,
+                     metavar="bfd-target-name")
 
-parser.add_argument ('inputfile', help='binary file', 
-                     default = None, nargs = '?') 
-                     
+parser.add_argument('inputfile', help='binary file',
+                     default=None, nargs = '?')
 
-parser.add_argument ('-q', '--quiet', help='disable verbosity.', 
-                     dest = "quiet", default = False, 
+
+parser.add_argument('-q', '--quiet', help='disable verbosity.',
+                     dest = "quiet", default = False,
                      action = "store_true")
-parser.add_argument ('-x', '--xml', help='preload an existing microcode in '
-                     'XML format.', 
+parser.add_argument('-x', '--xml', help='preload an existing microcode in '
+                     'XML format.',
                      dest = "xmlmc", metavar="microcode", default = None)
 
 #
 # aliases for debugger commands
-# 
-f = insight.debugger.binfile
-r = insight.debugger.run
-ms = insight.debugger.microstep
-s = insight.debugger.step
-c = insight.debugger.cont
-b = insight.debugger.breakpoint
-watch = insight.debugger.watchpoint
-d = insight.debugger.delete_breakpoints
-P = insight.debugger.prog
-pc = insight.debugger.pc
-ep = insight.debugger.entrypoint
-cond = insight.debugger.cond
+#
+aliases_ = { 'f' : insight.debugger.binfile,
+            'r' : insight.debugger.run,
+            'ms' : insight.debugger.microstep,
+            's' : insight.debugger.step,
+            'c' : insight.debugger.cont,
+            'b' : insight.debugger.breakpoint,
+            'w' : insight.debugger.watchpoint,
+            'd' : insight.debugger.delete_breakpoints,
+            'P' : insight.debugger.prog,
+            'pc' : insight.debugger.pc,
+            'ep' : insight.debugger.entrypoint,
+            'cond' : insight.debugger.cond }
 
+for a in aliases_.keys():
+    globals()[a] = aliases_[a];
+
+def aliases():
+    """Display current aliases of insgith db"""
+    for a in aliases_.keys():
+        print "{:10s} -> {}".format(a, aliases_[a].__name__)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     if not args.quiet:
         print banner
     if args.inputfile != None:
-        binfile (args.inputfile, args.target)
+        binfile(args.inputfile, args.target)
         if args.xmlmc != None and P() != None:
-            load_mc (args.xmlmc)
+            load_mc(args.xmlmc)
     try:
         from mydb import *
     except ImportError:
         pass
-    code.interact ("", local = globals ())
+    code.interact("", local = globals())
 
