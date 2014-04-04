c47e31eb374765fabf594f67fbae9039273f1c0c
point <point@00316523-1ed1-4fdc-9313-76483a3f41dc>
2014-04-04 18:08:16 +0000

    make linear-sweep memorize visited addresses.
    git-svn-id: https://insight.labri.fr/svn/trunk@774 00316523-1ed1-4fdc-9313-76483a3f41dc

--- src/analyses/cfgrecovery/LinearSweep.hh
+++ src/analyses/cfgrecovery/LinearSweep.hh
@@ -4,7 +4,7 @@
 # include <analyses/cfgrecovery/NullContext.hh>
 # include <analyses/cfgrecovery/MicrocodeAddressProgramPoint.hh>
 # include <analyses/cfgrecovery/AbstractStepper.hh>
-# include <analyses/cfgrecovery/DummyStateSpace.hh>
+# include <analyses/cfgrecovery/SingleContextStateSpace.hh>
 # include <analyses/cfgrecovery/AbstractMemoryTraversal.hh>
 
 class LinearSweep
@@ -13,7 +13,7 @@ public:
   typedef NullContext Context;
   typedef MicrocodeAddressProgramPoint ProgramPoint;
   typedef AbstractState<ProgramPoint, Context> State;
-  typedef DummyStateSpace<State> StateSpace;
+  typedef SingleContextStateSpace<State> StateSpace;
 
   class Stepper : public AbstractStepper<State> {
   public:
